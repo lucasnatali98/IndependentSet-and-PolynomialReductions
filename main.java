26b3c7f89c5b8800dada94b6718c9e5aacb024b8
Lucas <lucasnatali16@gmail.com>
2019-12-02 16:55:28 -0300

    medição do tempo

--- main.java
+++ main.java
@@ -6,29 +6,88 @@ public class main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		Grafo grafo = new Grafo("testeSat.txt");
-		
-		//Variáveis para fazer a medida do tempo
-		long inicio = System.currentTimeMillis();
-		long fim = System.currentTimeMillis();  
+		Grafo grafo = new Grafo("teste1.txt");
+		Grafo grafo1 = new Grafo("ConjuntoIndTeste1.txt");
+		Grafo grafo2 = new Grafo("ConjuntoIndTeste2.txt");
 		
 		
-		grafo.leSat("testeSat.txt");
-		System.out.println();
-	
-		/*
+		
+		
+
+		
+		
+		System.out.println("Instâncias do Conjunto Independente");
+		//Primeira instância do C.I
+		long inicio = System.nanoTime();
 		int[] conjunto = grafo.conjuntoIndependente();
-		int[] clique = grafo.clique();
+		System.out.println();
+		for(int i = 0; i<conjunto.length; i++)
+			System.out.print(conjunto[i]+ "|");
+		System.out.println();
+		conjunto = null;
+		long fim = System.nanoTime();
+		System.out.println("Tempo de execução - C.I: " + (fim -inicio) + "ns");
+		System.out.println();
 		
+		//Segunda instância do C.I
+		long inicio2 = System.nanoTime();
+		conjunto = grafo1.conjuntoIndependente();
+		for (int i = 0; i < conjunto.length; i++) {
+			System.out.print(conjunto[i] + "|");
+		}
+		System.out.println();
+		long fim2 = System.nanoTime();
+		System.out.println("Tempo de execução - C.I (2): " + (fim2 -inicio2) + "ns");
+		conjunto = null;
+		System.out.println();
+		
+		long inicio3 = System.nanoTime();
+		conjunto = grafo2.conjuntoIndependente();
+		for (int i = 0; i < conjunto.length; i++) {
+			System.out.print(conjunto[i] + "|");
+		}
+		System.out.println();
+		long fim3 = System.nanoTime();
+		System.out.println("Tempo de execução - C.I (3): " + (fim3-inicio3) + "ns");
+		conjunto = null;
+		System.out.println();
+		
+		System.out.println("Instâncias do clique");
+		
+		inicio = System.nanoTime();
+		int[] clique = grafo.clique();
 		for(int i=0; i<clique.length; i++)
 		{
 			System.out.print(clique[i] + "|");
 		}
-		
+		fim = System.nanoTime();
 		System.out.println();
-		for(int i = 0; i<conjunto.length; i++)
-			System.out.print(conjunto[i]+ "|");
-			
-		 */
+		System.out.println("Tempo de execução: - Clique: " + (fim - inicio) + "ns");
+		System.out.println();
+		clique = null;
+		
+		inicio2 = System.nanoTime();
+		clique = grafo1.clique();
+		for(int i=0; i<clique.length; i++)
+		{
+			System.out.print(clique[i] + "|");
+		}
+		fim2 = System.nanoTime();
+		System.out.println();
+		System.out.println("Tempo de execução: - Clique (2): " + (fim2 - inicio2) + "ns");
+		System.out.println();
+		clique = null;
+		
+		inicio3 = System.nanoTime();
+		clique = grafo2.clique();
+		for(int i=0; i<clique.length; i++)
+		{
+			System.out.print(clique[i] + "|");
+		}
+		fim3 = System.nanoTime();
+		System.out.println();
+		System.out.println("Tempo de execução: - Clique (3): " + (fim3 - inicio3) + "ns");
+		System.out.println();
+		clique = null;
 	}
 }
